5f16eca3259dd24a4c3b869c6a041ee66682563a
samsail <samsail@live.com.au>
2021-01-05 20:44:31 +1100

    looks better

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import math
 from pygame.locals import *
 from tkinter import *
-
+from random import randint
 
 pygame.init()
 running = True
@@ -27,6 +27,13 @@ T = T_calculated/100000 #period of orbit (perhaps x10^5 or smth, as otherwise it
 M = 10 #[user input]
 m = 10 #[user input]
 
+stars = []
+
+def genStars():
+    for i in range(150):
+        stars.append((randint(0, simSize[0]), randint(0, simSize[1])))
+
+genStars()
 
 #display loop
 while running == True:
@@ -49,10 +56,15 @@ while running == True:
 
     #resizing the window: [DO WE WANT IT SCALABLE? it might mess with our program...]
         if event.type == VIDEORESIZE:
+            print("event")
             screenSize = (event.w,event.h)
             menuSize = (menuSize[0],event.h)
             simSize = (screenSize[0] - menuSize[0], screenSize[1])
             screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            stars = []
+            genStars()
+    
+
     
     simSurface = pygame.Surface((screenSize[0]-menuSize[0],screenSize[1]))
     menuSurface = pygame.Surface(menuSize)
@@ -61,15 +73,20 @@ while running == True:
     
 
 
-    simSurface.fill((255,255,255))
-    menuSurface.fill((125,125,125,125))   
+    simSurface.fill((0,0,0))
+    menuSurface.fill((125,125,125,125)) 
+    for i in range(150):
+        pygame.draw.circle(simSurface, (255,255,255), stars[i], 1)  
     centre = (simSize[0]/2,simSize[1]/2)
     rad = simSize[0]/5
     for baseAngle in range(0, 2*math.floor(math.pi*1000), 40):
         angle = baseAngle/1000
         pygame.draw.circle(simSurface, [0,0,0,255],(centre[0] + rad*math.cos(angle),centre[1] + rad*math.sin(angle)),2)
-    pygame.draw.circle(simSurface, [0, 0, 0, 255], (centre[0] + rad*math.cos(t), centre[1]+rad*math.sin(t)), 30)
-    pygame.draw.circle(simSurface, [0, 0, 0, 255], centre, 40)
+    pygame.draw.circle(simSurface, [187,187,187, 255], (centre[0] + rad*math.cos(t), centre[1]+rad*math.sin(t)), 30)
+    pygame.draw.circle(simSurface, [30,30,255, 255], centre, 40)
+    pygame.draw.line(simSurface, [255, 255, 255, 255], (centre[0] + rad*math.cos(t), centre[1]+rad*math.sin(t)), centre, width = 3)
+
+
     screen.blit(simSurface,(0,0))
     screen.blit(menuSurface,(screenSize[0]-menuSize[0],0))
 
