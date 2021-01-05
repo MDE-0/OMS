2be60c357b6a49eb9572dbba50f16b121ff249ca
Dhruv <dhruv6upt4@gmail.com>
2021-01-05 16:32:17 +1100

    created main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+import tkinter
+
+
+#pygame display
+
+
+
+
+
+#planet class
+
+
+
+
+
+#Settings menu (Located top right of the program), which includes options such as changing variables, and includes formulae values and information about the orbit/s
+
+
+
+
+
+
+#Hey, Sam, if we get this to work, we could build more simulations ahahaha.. could even make a website to collate them; but im probs being too ambitious rn LOL
