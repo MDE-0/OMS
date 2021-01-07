7e15903ce7088dba42753e7ad1239ec64e8ac05a
samsail <samsail@live.com.au>
2021-01-07 15:02:10 +1100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ pygame.display.set_caption("Interactive Physics Orbital Mechanics Simulation (IP
 screen.fill((0, 0, 0))
 
 #Python GUI menu
-manager = pygame_gui.UIManager(menuSize)
+manager = pygame_gui.UIManager(screenSize)
 clock = pygame.time.Clock()
 
 
@@ -39,12 +39,13 @@ def genStars():
         stars.append((randint(0, simSize[0]), randint(0, simSize[1])))
 
 class button(object):
-        def __init__(self, var, y_placement):
-            self.var = var
-            self.var_name = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0,y_placement*50), (120, 50)), text = f"{vars[self.var][0]}", manager = manager)
-            self.increase = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((120,y_placement*50), (50, 50)), text = "↑", manager = manager)
-            self.decrease = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((170, y_placement*50), (50, 50)), text = "↓", manager = manager)
-            self.value = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((220, y_placement*50), (100, 50)), text = f"{vars[self.var][1]}", manager = manager)
+    def __init__(self, var, y_placement):
+        self.var = var
+        self.y_placement = y_placement
+        self.var_name = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((simSize[0] + 0,y_placement*50), (120, 50)), text = f"{vars[self.var][0]}", manager = manager)
+        self.increase = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((simSize[0] + 120,y_placement*50), (50, 50)), text = "↑", manager = manager)
+        self.decrease = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((simSize[0] + 170, y_placement*50), (50, 50)), text = "↓", manager = manager)
+        self.value = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((simSize[0] + 220, y_placement*50), (100, 50)), text = f"{vars[self.var][1]}", manager = manager)
         
             
 ind = 0
@@ -80,23 +81,26 @@ while running == True:
             genStars()
         elif event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_BUTTON_PRESSED:
             for but in buttons:
-                if event.ui_element == but.increase:
-                    vars[but.var] += 5
+                if event.ui_element == but.increase: 
+                    vars[but.var][1] += 5
+                    but.value.kill()
+                    but.value = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((simSize[0] + 220, but.y_placement*50), (100, 50)), text = f"{vars[but.var][1]}", manager = manager)
                 elif event.ui_element == but.decrease:
-                    vars[but.var] += 5
-        print(event.type)
+                    vars[but.var][1] -= 5
+                    but.value.text = f"{vars[but.var][1]}"
+                    but.value.kill()
+                    but.value = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((simSize[0] + 220, but.y_placement*50), (100, 50)), text = f"{vars[but.var][1]}", manager = manager)
         manager.process_events(event)
     
     
     
     
     simSurface = pygame.Surface((screenSize[0]-menuSize[0],screenSize[1]))
-    menuSurface = pygame.Surface(menuSize)
+ 
 
 
-
+    screen.fill((200,200,200,200))
     simSurface.fill((0,0,0))
-    menuSurface.fill((200,200,200,200)) 
     
     
     
@@ -107,12 +111,12 @@ while running == True:
     
     centre = (simSize[0]/2,simSize[1]/2)
 
-    rad = simSize[0]/5
+
     radius_planet = 40
     radius_satellite = 30
     for baseAngle in range(0, 2*math.floor(math.pi*1000), 40):
         angle = baseAngle/1000
-        pygame.draw.circle(simSurface, [173,255,47,255],(centre[0] + rad*math.cos(angle),centre[1] + rad*math.sin(angle)), 1)
+        pygame.draw.circle(simSurface, [173,255,47,255],(centre[0] + vars["r"][1]*math.cos(angle),centre[1] + vars["r"][1]*math.sin(angle)), 1)
 
     #draw_arrow function
 
@@ -127,14 +131,14 @@ while running == True:
 
     
     #BELOW FUNCTION WORKS WITH draw_arrow function
-    pygame.draw.arrow(simSurface, [255, 255, 255, 255], (centre[0] + rad*math.cos(t), centre[1]+rad*math.sin(t)), (centre[0] + radius_planet*math.cos(t), centre[1]+radius_planet*math.sin(t)))
+    pygame.draw.arrow(simSurface, [255, 255, 255, 255], (centre[0] + vars["r"][1]*math.cos(t), centre[1]+vars["r"][1]*math.sin(t)), (centre[0] + radius_planet*math.cos(t), centre[1]+radius_planet*math.sin(t)))
     
     offsetAngle = 0.5
-    pygame.draw.arrow(simSurface, [255, 255, 255, 255], (centre[0] + rad*math.cos(t), centre[1]+rad*math.sin(t)), (centre[0] + rad*1/math.cos(offsetAngle)*math.cos(t + offsetAngle), centre[1] + rad*1/math.cos(offsetAngle)*math.sin(t + offsetAngle)))
+    pygame.draw.arrow(simSurface, [255, 255, 255, 255], (centre[0] + vars["r"][1]*math.cos(t), centre[1]+vars["r"][1]*math.sin(t)), (centre[0] + vars["r"][1]*1/math.cos(offsetAngle)*math.cos(t + offsetAngle), centre[1] + vars["r"][1]*1/math.cos(offsetAngle)*math.sin(t + offsetAngle)))
     ### WHEN F_g BUTTON COMES INTO CONTACT WITH MOUSE POINTER, CHANGE COLOUR OF ARROW (HIGHLIGHTING EFEFCT)
 
-    pygame.draw.circle(simSurface, [187,187,187, 255], (centre[0] + rad*math.cos(t), centre[1]+rad*math.sin(t)), radius_satellite)
-
+    pygame.draw.circle(simSurface, [187,187,187, 255], (centre[0] + vars["r"][1]*math.cos(t), centre[1]+vars["r"][1]*math.sin(t)), radius_satellite)
+    
     
     #defining buttons
     
@@ -149,8 +153,7 @@ while running == True:
 
     #I'm not sure why this isn't working....
     screen.blit(simSurface,(0,0))
-    manager.draw_ui(menuSurface)
-    screen.blit(menuSurface,(screenSize[0]-menuSize[0],0))
+    manager.draw_ui(screen)
     
 
     # manager.set_visual_debug_mode(True)
